f1082abfa944e429e9047070c72cae30db18bfd2
duncanturk <31703168+duncanturk@users.noreply.github.com>
2018-02-04 20:29:33 +0100

    checkOutdated() function
    remove logging

--- youtube-skip.js
+++ youtube-skip.js
@@ -6,22 +6,25 @@
 
 var interval;
 var notFoundCounter = 0;
+
+function checkOutdated(){
+    if(notFoundCounter++ > 120)
+        alert("YouYube-Skip-Skript may be outdated");
+}
+
 function checkLoaded(){
-    console.log("checking");
     if($('div#top-level-buttons.style-scope.ytd-menu-renderer '+
         'ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.style-text.force-icon-button '+
         'a.style-scope.ytd-toggle-button-renderer '+
         'yt-icon-button#button.style-scope.ytd-toggle-button-renderer.style-text').length>0){
         if($('#top-level-buttons>:nth-child(2) *[aria-pressed="false"]').length < 1)
-            alert("YouYube-Skip-Skript may be outdated");
+            checkOutdated();
         if($('#top-level-buttons>:nth-child(2) *[aria-pressed="true"]').length >0){
             console.log("skipped");
             $(".ytp-next-button")[0].click();
         }
         notFoundCounter=0;
-    }
-    else if(notFoundCounter++ > 120)
-        alert("YouYube-Skip-Skript may be outdated");
+    } else checkOutdated();
 }
 
 function startInterval(){
